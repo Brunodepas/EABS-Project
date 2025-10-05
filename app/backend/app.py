02eded3958b4fe7c8d379b183f4647b35c9c75b2
agusc546 <aguscesari14@gmail.com>
2025-10-04 22:15:57 -0300

    actualizo app.py

--- app/backend/app.py
+++ app/backend/app.py
@@ -1,5 +1,10 @@
 from flask import Flask, request, jsonify
+from model import predict_plant
+import base64
 import numpy as np
+from PIL import Image
+import io
+
 
 app = Flask(__name__)
 
@@ -10,10 +15,15 @@ def index():
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json
-    # ejemplo simple: devuelve el doble de un número
-    x = data.get('value', 0)
-    result = x * 2
-    return jsonify({"result": result})
+    image_base64 = data.get("image")
+    if not image_base64:
+        return jsonify({"error": "No se envió ninguna imagen"}), 400
+
+    try:
+        prediction = predict_plant(image_base64)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    return jsonify({"result": prediction})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
