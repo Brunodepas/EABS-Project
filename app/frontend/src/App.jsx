fc8c42e2baec8e47652fe2541ea5c13b606108d6
Emiliano Bernal <emiibernal1275@gmail.com>
2025-11-13 18:47:04 -0300

    Cambios en el app para introducir la nueva page del register

--- app/frontend/src/App.jsx
+++ app/frontend/src/App.jsx
@@ -1,15 +1,11 @@
-//Esto trae las herramientas principales de React Router, la libreria que permite navegar entre distintas pags
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import MainLayout from "./components/MainLayout";
 import ImageUpload from "./pages/ImageUpload";
-//import Register from "./pages/Register";
-//import ForgotPassword from "./pages/ForgotPassword";
-//import History from "./pages/History";
-//import Account from "./pages/Account";
-//import Config from "./pages/Config";
+import Register from "./pages/Register";
+import { useLocation } from "react-router-dom";
 
-//Función para proteger rutas privadas
+// Función para proteger rutas privadas
 function PrivateRoute({ children }) {
   const token = localStorage.getItem("token");
   if (!token) {
@@ -18,20 +14,17 @@ function PrivateRoute({ children }) {
   return children;
 }
 
-
-function app() {
-  return(
+function App() {
+  return (
     <Router>
       <Routes>
-        
-        {/*Rutas publicas*/}
+
+        {/* Rutas públicas */}
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
-        {/* <Route path="/register" element={<Register />} /> */}
-        {/* <Route path="/forgot-password" element={<ForgotPassword />} /> */}
+        <Route path="/register" element={<Register />} />
 
-        {/*Rutas Privadas */}
-        
+        {/* Rutas privadas */}
         <Route
           path="/image-upload"
           element={
@@ -40,41 +33,10 @@ function app() {
             </MainLayout>
           }
         />
-        
-        {/* <Route */}
-          {/* path="/history" */}
-          {/* element={ */}
-            {/* <MainLayout> */}
-              {/* <History /> */}
-            {/* </MainLayout> */}
-          {/* } */}
-        {/* /> */}
-        
-        {/* <Route */}
-          {/* path="/cuenta" */}
-          {/* element={ */}
-            {/* <PrivateRoute> */}
-              {/* <MainLayout> */}
-                {/* <Cuenta /> */}
-              {/* </MainLayout> */}
-            {/* </PrivateRoute> */}
-          {/* } */}
-        {/* /> */}
 
-        {/* <Route */}
-          {/* path="/configuracion" */}
-          {/* element={ */}
-            {/* <PrivateRoute> */}
-              {/* <MainLayout> */}
-                {/* <Configuracion /> */}
-              {/* </MainLayout> */}
-            {/* </PrivateRoute> */}
-          {/* } */}
-        {/* /> */}
-      
       </Routes>
     </Router>
   );
 }
 
-export default app;
+export default App;
