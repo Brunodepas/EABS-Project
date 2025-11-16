f8b59f28d31375972fe3f99fe8933eaa30e44da0
S4m0912 <saamuel.quintero@gmail.com>
2025-11-16 12:43:35 -0300

    Test de boton de Analizar otra planta

--- app/frontend/src/pages/__tests__/ImageUpload.test.jsx
+++ app/frontend/src/pages/__tests__/ImageUpload.test.jsx
@@ -14,6 +14,8 @@ test("la página se inicializa correctamente", () => {
   ).toBeInTheDocument();
 });
 
+
+
 test("cargar una imagen y mostrar los resultados", async () => {
   // mockeamos fileReader para leer una imagen ficticia
   class MockFileReader {
@@ -61,6 +63,75 @@ test("cargar una imagen y mostrar los resultados", async () => {
   ).toBeInTheDocument();
 
   expect(screen.getByText(/90%/i)).toBeInTheDocument();
+
   expect(screen.getByText(/Evitar el riego por aspersión y aplicar bactericidas a base de cobre./i)).toBeInTheDocument();
+
+});
+
+test("probamos el boton Analizar otra planta", async () => {
+
+  class MockFileReader {
+    constructor() {
+      this.onloadend = null;
+      this.result = null;
+    }
+    readAsDataURL() {
+      this.result = "data:image/png;base64,MOCK_IMAGE";
+      this.onloadend && this.onloadend();
+    }
+  }
+  global.FileReader = MockFileReader;
+
+  global.fetch = vi.fn(() =>
+    Promise.resolve({
+      json: () =>
+        Promise.resolve({
+          planta: "Tomate",
+          enfermedad: "Mancha bacteriana del tomate",
+          confianza: "90%",
+          recomendacion:
+            "Evitar el riego por aspersión y aplicar bactericidas a base de cobre.",
+        }),
+    })
+  );
+
+  render(<ImageUpload />);
+
+  const fileInput = screen.getByLabelText(/Seleccionar imagen/i, {selector: "input",});
+
+  const fakeFile = new File(["dummy"], "plantita.png", { type: "image/png" });
+
+  fireEvent.change(fileInput, { target: { files: [fakeFile] } });
+
+  // Resultados de la imagen subida
+
+    expect(await screen.findByRole("heading",{ name: /Tomate/i }) //hay emoji antes
+  ).toBeInTheDocument();
+
+  expect(screen.getByText(/Mancha bacteriana del tomate/i)).toBeInTheDocument();
+
+  expect(screen.getByText(/90%/i)).toBeInTheDocument();
+  
+  expect(screen.getByText(/Evitar el riego por aspersión y aplicar bactericidas a base de cobre./i)).toBeInTheDocument();
+
+  const AnalizarOtra = screen.getByRole("button", {name: /Analizar otra planta/i,});
+
+  fireEvent.click(AnalizarOtra); // probamos el boton 
+
+  // Esperamos que se vea asi 
+
+  expect(screen.getByText(/Sube una foto de tu planta/i)).toBeInTheDocument();
+
+  expect(screen.getByText(/Seleccionar imagen/i)).toBeInTheDocument();
+
+  // Los resultados anteriores debe desaparecer
+
+  expect(screen.queryByText(/Tomate/i)).not.toBeInTheDocument();
+
+  expect(screen.queryByText(/90%/i)).not.toBeInTheDocument();
+
+  expect(screen.queryByText(/Mancha bacteriana del tomate/i)).not.toBeInTheDocument();
+
+  expect(screen.queryByText(/Evitar el riego por aspersión y aplicar bactericidas a base de cobre./i)).not.toBeInTheDocument();
 
 });
