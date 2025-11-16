c70eb4d902f14a6be98d691ad190021a3303101e
S4m0912 <saamuel.quintero@gmail.com>
2025-11-16 17:00:01 -0300

    Test para email o contrasenia incorrectos en Login

--- app/frontend/src/pages/__tests__/Login.test.jsx
+++ app/frontend/src/pages/__tests__/Login.test.jsx
@@ -1,6 +1,12 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent, cleanup } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import Login from "../Login";
+import { vi, afterEach } from "vitest";
+
+afterEach(() => {
+  cleanup();
+  vi.clearAllMocks();
+});
 
 //TEST INICIALIZACION
 
@@ -54,6 +60,54 @@ test("muestra alerta si los campos están vacíos", () => {
   fireEvent.click(BotonEntrar);
 
   expect(alertMock).toHaveBeenCalledWith("Completa todos los campos.");
-  
+
 });
+
+//TEST EMAIL O CONTRASENIA INCORRECTOS
+
+test("muestra alerta si email o contrasenia son incorrectos", async () => {
+  const alertMock = vi.spyOn(window, "alert").mockImplementation(() => {});
+
+  global.fetch = vi
+    .fn()
+    .mockResolvedValueOnce({
+      ok: false,
+      json: () =>
+        Promise.resolve({
+          error: "Usuario o contraseña incorrectos",
+        }),
+    })
+    .mockResolvedValueOnce({
+      ok: false,
+      json: () =>
+        Promise.resolve({
+          error: "Usuario o contraseña incorrectos",
+        }),
+    });
+
+  render(
+    <MemoryRouter>
+      <Login />
+    </MemoryRouter>
+  );
+
+  fireEvent.change(screen.getByPlaceholderText("Correo electrónico"), {
+    target: { value: "test@mail.com" },
+  });
+
+  fireEvent.change(screen.getByPlaceholderText("Contraseña"), {
+    target: { value: "123456" },
+  });
+
+  fireEvent.click(screen.getByRole("button", { name: /entrar/i }));
+
+  await Promise.resolve();
+  await Promise.resolve(); 
+
+  expect(alertMock).toHaveBeenCalledWith(
+    "Usuario o contraseña incorrectos"
+
+  );
+});
+
 
